1b123fedfe9b3146ca1b9dcca9cbe1801e94f454
Max Salewski <maxsalewski@gmail.com>
2021-06-16 19:00:15 -0300

    Teste requisito 17

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const btnlogin = document.getElementById('btn-login');
+const btnLogin = document.getElementById('btn-login');
 const login = document.getElementById('login');
 const password = document.getElementById('password');
+const agreement = document.getElementById('agreement');
+const btnSubmit = document.getElementById('submit-btn');
 
-btnlogin.addEventListener('click', () => {
+btnLogin.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
 });
+
+function CheckAgreement() {     
+  const checkAgreement = document.getElementById('agreement');     
+  checkAgreement.addEventListener('change', () => {     
+    if (checkAgreement.checked) {
+           ActivateButton();     } 
+           else {     
+             DeactivateButton();     
+            }     
+          });   
+        }
+
+function DeactivateButton() {
+       submitBtn.disabled = true;
+    }
+function ActivateButton() {
+       submitBtn.disabled = false;  
+    }
